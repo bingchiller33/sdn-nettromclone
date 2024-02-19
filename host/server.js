2d7c1a0f6fc3edd5e8f6268434946bbc2b48839b
bingchiller33 <97215217+bingchiller33@users.noreply.github.com>
2024-02-19 22:04:50 +0700

    commiting

---
+++ host/server.js
@@ -0,0 +1,29 @@
+// using module 'express' to initialize a web server
+import express, {json} from "express";
+import * as dotenv from "dotenv";
+import {productRouter} from "./routes/index.js";
+import connectDB from "./database.js";
+import cors from 'cors';
+dotenv.config();
+
+// tạo 1 constant 'app' đại diện cho server express trong ứng dụng
+const app = express();
+// thêm middleware
+app.use(json());
+app.use(cors())
+// kích hoạt hoạt động định tuyến (routing) cho các request của client
+app.get("/", (req, res) => {
+  
+  res.send(`<h1 style="color:red">Welcome to homepage!</h1>`);
+});
+
+app.use("/products", productRouter);
+
+const port = process.env.PORT || 9999;
+
+// lắng nghe các request gửi tới web server tại port 9999
+app.listen(port, async () => {
+  connectDB();
+  console.log(`Web server listening on http://localhost:${port}`);
+});
+
