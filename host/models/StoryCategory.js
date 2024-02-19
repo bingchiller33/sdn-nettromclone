10aee447c8c6265db23ded9d9f08d20579d11051
Macbeth2k <anhvdhe160063@fpt.edu.vn>
2024-02-19 23:20:47 +0700

    add model for Comment, Reply and StoryCategory

---
+++ host/models/StoryCategory.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const storyCategorySchema = new Schema({
+  categoryId: {
+    type: Schema.Types.ObjectId, 
+    required: true,
+    ref: 'Comments' 
+  },
+  storyId: {
+    type: Schema.Types.ObjectId, 
+    required: true,
+    ref: 'Stories' 
+  },
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  isActive: {
+    type: Boolean,
+    default: false
+  },
+},{
+  timestamps: true
+});
+
+const StoryCategories = mongoose.model('StoryCategories', storyCategorySchema);
+
+module.exports = StoryCategories;
