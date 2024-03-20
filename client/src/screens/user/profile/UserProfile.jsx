4a807188c350a4651ba67e464b39d5395d4a7793
Luu Danh Luong <phamluong6866@gmail.com>
2024-03-20 08:10:35 +0700

    update profile view list

--- client/src/screens/user/profile/UserProfile.jsx
+++ client/src/screens/user/profile/UserProfile.jsx
@@ -3,8 +3,11 @@ import UserDetails from "../../../components/user/UserDetails";
 import { BASE_URL } from "../../../components/common/utilities/initials";
 import axios from "axios";
 import UserContext from "../../../contexts/UserContext.js";
+import { fetchStoriesSuccess } from "../../../components/common/data/dataStory/dataSlice.js";
+import { useDispatch } from "react-redux";
 
 const UserProfile = () => {
+  const dispatch = useDispatch();
   const { user, setUser } = useContext(UserContext);
   const jwt = localStorage.getItem("token");
   const config = {
@@ -23,6 +26,13 @@ const UserProfile = () => {
         .catch((e) => console.log(e.message));
     }
   }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/story/get_list_stories`, config)
+      .then((res) => dispatch(fetchStoriesSuccess(res.data)))
+      .catch((e) => console.log(e.message));
+  }, [dispatch]);
 
   return <UserDetails />;
 };
